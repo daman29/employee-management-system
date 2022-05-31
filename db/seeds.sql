9810708464f9449d8f6aa0e586fa565803b07389
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-31 17:23:21 +0930

    added use of promise for DB queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,41 @@
 INSERT INTO department (name)
 VALUES ('Engineering'),
     ('Admin'),
-    ('Leaders');
-
+    ('HR'),
+    ('Finance'),
+    ('C-Level');
 INSERT INTO role(title, salary, department_id)
 VALUES ('Junior Engineer', 70000, 1),
     ('Senior Engineer', 100000, 1),
     ('Office Admin', 55000, 2),
-    ('HR Admin', 65000, 2),
-    ('Lead Engineer', 130000, 3),
+    ('HR Admin', 65000, 3),
+    ('Lead Engineer', 130000, 1),
+    ('Accounts Manager', 130000, 4),
     ('HR Manager', 130000, 3),
-    ('General Manager', 150000, 3);
-
+    ('Managing Director', 210000, 5);
 INSERT INTO employee(first_name, last_name, role_id)
 VALUES ('Fion', 'Hirsch', 1),
-('Fion', 'Hirsch', 1),
-('Fion', 'Hirsch', 2),
-('Fion', 'Hirsch', 3),
-('Fion', 'Hirsch', 4),
-('Fion', 'Hirsch', 4),
-('Fion', 'Hirsch', 5),
-('Fion', 'Hirsch', 6),
-('Fion', 'Hirsch', 7);
+    ('Caelie', 'Colton', 1),
+    ('Amber', 'Jacobson', 2),
+    ('Oli', 'Traves', 2),
+    ('Isdore', 'Underwood', 3),
+    ('Ennis', 'Brie', 4),
+    ('Riannon', 'Denman', 4),
+    ('Bishop', 'Fleming', 5),
+    ('Eleanora', 'Kinsey', 6),
+    ('Reynard', 'Michaels', 6),
+    ('Stacey', 'Varnham', 7),
+    ('Lyn', 'Alfredson', 8);
+UPDATE employee
+SET manager_id = 8
+WHERE role_id = 1
+    OR role_id = 2;
+UPDATE employee
+SET manager_id = 11
+WHERE role_id = 3
+    OR role_id = 4;
+UPDATE employee
+SET manager_id = 12
+WHERE role_id = 5
+    OR role_id = 6
+    OR role_id = 7;
--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const mysql = require("mysql2");
+require("dotenv").config();
 const inquirer = require("inquirer");
-const cTable = require("console.table")
-const DbQueries = require("./lib/Queries")
+const cTable = require("console.table");
+const mysql = require("mysql2/promise");
+const DbQuery = require("./lib/DbQuery");
+
+async function main() {
+  const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  const queries = new DbQuery(db);
+//   queries.selectDB();
+  await queries.viewAllDepartments(db);
+}
+
+main();
